7a443e51efe21b3df79f02a8d218c1829eede778
April Copes <antdevelopment1@gmail.com>
2019-05-04 22:24:23 -0600

    adds all files examples

---
+++ advancedComponentUsage/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'quote'">Quote</button>
+                <button @click="selectedComponent = 'author'">Author</button>
+                <button @click="selectedComponent = 'new'">New</button>
+                <hr>
+                <p>{{selectedComponent}}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+               <!-- <quote>
+                   <h2 slot="title">{{quoteTitle}}</h2>
+                   <p>{{quoteBody}}</p>  
+               </quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+    export default {
+        data() {
+            return {
+                quoteTitle: "The Final Quote",
+                quoteBody: "Given a sign, what direction would you go?",
+                selectedComponent: 'quote'
+            }
+        },
+        components: {
+            Quote,
+            Author,
+            New
+        }   
+    }
+</script>
+
+<style>
+    p {
+        color: lightblue;
+    }
+</style>
---
+++ advancedComponentUsage/src/components/New.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{counter}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "new-component",
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    destroyed() {
+        console.log('Destroyed')
+    },
+    deactivated() {
+        console.log('Deactivated')
+    },
+    activated() {
+        console.log('activated')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
