ee3d3b98b636a3938f5b115fe1c04d540e12357d
April Copes <antdevelopment1@gmail.com>
2019-05-04 22:23:52 -0600

    examples of using a directive and passing it a value

---
+++ directives/my-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+    </div>
+	<div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight="'red'">Color this</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+
+</style>
+
+
+
