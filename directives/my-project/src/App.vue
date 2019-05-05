285d9e2c5d42228049aa163e833b8252e774806f
April Copes <antdevelopment1@gmail.com>
2019-05-05 17:14:55 -0600

    using multiple modifiers to create a blinking effect

--- directives/my-project/src/App.vue
+++ directives/my-project/src/App.vue
@@ -9,7 +9,7 @@
     </div>
 	<div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-		<p v-local-highlight:background.delayed="'red'">Color This</p>
+		<p v-local-highlight:background.delayed.blink="'red'">Color This</p>
         <p v-highlight:background.delayed="'red'">Custom Directives</p>
       </div>
     </div>
@@ -21,17 +21,33 @@ export default {
 	directives: {
 		 'local-highlight': {
 			 bind(el, binding, vnode) {
-				 let delay = 0;
-    if (binding.modifiers['delayed']) {
-      delay = 3000;
-    }
-    setTimeout( () => {
-      if (binding.arg == 'background') {
-        el.style.backgroundColor = binding.value;
-      } else {
-        el.style.color = binding.value;
-      }
-    }, delay)
+				let delay = 0;
+				if (binding.modifiers['delayed']) {
+					delay = 3000;
+				}
+				if (binding.modifiers['blink']) {
+					let mainColor = binding.value;
+					let secondColor = 'blue';
+					let currentColor = mainColor;
+					setTimeout( () => {
+						setInterval(() => {
+							currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+							if (binding.arg == 'background') {
+								el.style.backgroundColor = currentColor;
+							} else {
+								el.style.color = currentColor;
+							}
+						} ,1000)
+					}, delay)
+				} else {
+					setTimeout( () => {
+						if (binding.arg == 'background') {
+							el.style.backgroundColor = binding.value;
+						} else {
+							el.style.color = binding.value;
+						}
+					}, delay)
+				}
 			 }
 		 }
 	}
