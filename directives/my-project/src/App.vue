bda8a91eeab57dd429da58c5a1203e7ba03c4749
April Copes <antdevelopment1@gmail.com>
2019-05-05 16:54:09 -0600

    example of directives being registered locally

--- directives/my-project/src/App.vue
+++ directives/my-project/src/App.vue
@@ -9,10 +9,8 @@
     </div>
 	<div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1 v-highlight>Custom Directives</h1>
-        <p v-highlight:background="'red'">Color this</p>
-        <p v-highlight="'red'">Color this</p>
-        <p v-highlight:background.delayed="'red'">Color this</p>
+		<p v-local-highlight:background.delayed="'red'">Color This</p>
+        <p v-highlight:background.delayed="'red'">Custom Directives</p>
       </div>
     </div>
   </div>
@@ -20,7 +18,23 @@
 
 <script>
 export default {
-	
+	directives: {
+		 'local-highlight': {
+			 bind(el, binding, vnode) {
+				 let delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout( () => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay)
+			 }
+		 }
+	}
 }
 </script>
 
