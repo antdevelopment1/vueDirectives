d9c2022fbaa04a41bb20c8067b7a6324af644874
April Copes <antdevelopment1@gmail.com>
2019-05-05 18:27:28 -0600

    example of using filters and changing filters

---
+++ filters/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters and Mixins</h1>
+        <p>{{ text | toUppercase | to-lowercase}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      text: "hello there!"
+    }
+    
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
