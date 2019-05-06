e95875fb4ed8d3c229d59468d5ee22417b19d9e8
April Copes <antdevelopment1@gmail.com>
2019-05-05 18:40:33 -0600

    Adds example of filtering words that contain the letter beingtyped inthe  input

--- filters/src/App.vue
+++ filters/src/App.vue
@@ -4,6 +4,11 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters and Mixins</h1>
         <p>{{ text | toUppercase | to-lowercase}}</p>
+        <hr>
+        <input v-model="fruitsText">
+        <ul>
+          <li v-for="fruit in filteredFruits">{{fruit}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -14,13 +19,22 @@ export default {
   name: 'app',
   data () {
     return {
-      text: "hello there!"
+      text: "hello there!",
+      fruits:["Apple", "Banana", "Mango", "Melon"],
+      fruitsText: ''
     }
     
   },
   filters: {
     toUppercase(value) {
       return value.toUpperCase();
+    }
+  },
+  computed: {
+    filteredFruits()  {
+      return this.fruits.filter((element)=> {
+        return element.match(this.fruitsText);
+      });
     }
   }
 }
