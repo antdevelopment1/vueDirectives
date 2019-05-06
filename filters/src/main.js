a949615b6af236e9037f344ea7ac63c102f7672c
April Copes <antdevelopment1@gmail.com>
2019-05-05 19:45:47 -0600

    Adds example of global mixins keeping in mind global minis effect all instances

--- filters/src/main.js
+++ filters/src/main.js
@@ -5,6 +5,13 @@ Vue.filter('to-lowercase', function(value) {
   return value.toLowerCase();
 })
 
+// Global mixin used everywhere
+Vue.mixin({
+  created() {
+    console.log('Global Mixin - Created Hook')
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
