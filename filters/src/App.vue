0c0739d756bd42287d0a0f82d8e44d0f6e456ba8
April Copes <antdevelopment1@gmail.com>
2019-06-26 17:51:48 -0400

    plays with excersies for mixins and filters

--- filters/src/App.vue
+++ filters/src/App.vue
@@ -3,41 +3,75 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters and Mixins</h1>
-        <p>{{ text | toUppercase | to-lowercase}}</p>
+
+        <!-- Build a global filter which reverses the text it is applied on -->
+        <p>{{firstText | reverse}}</p>
+        <!-- Build a global filter which counts the length of a word and appends it -->
+        <p>{{ secondText | calculateLength }}</p>
+        <!-- Do the same as in Excersies 1 and 2, now with computed Properties -->
+        <p>{{reverse}}</p>
+        <p>{{ lengthAware }}</p>
+        <!-- Share the computed Property rebuilding excersies 2 via a mixin -->
+
+        <!-- Not a part of the above sections -->
+        <!-- <p>{{ text | toUppercase | to-lowercase}}</p>
         <hr>
         <input v-model="filterText">
         <ul>
           <li v-for="(fruit, index) in filteredFruits" :key="index">{{fruit}}</li>
         </ul>
         <hr>
-        <list></list>
+        <list></list>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import List from  './List';
-import {fruitMixin} from './fruitMixin.js';
+// import List from "./List";
+// import { fruitMixin } from "./fruitMixin.js";
+import { lengthAwareMixin } from "./lengthAwareMixin";
 export default {
-  name: 'app',
-  mixins: [fruitMixin],
-  data () {
+  name: "app",
+  mixins: [],
+  data() {
     return {
-      text: "hello there!"
-    } 
-  },
-  components: {
-    List
+      firstText: "Some text",
+      secondText: "April"
+    };
   },
   filters: {
-    toUppercase(value) {
-      return value.toUpperCase();
+    reverse(value) {
+      return value
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  computed: {
+    reversed() {
+      return this.firstText
+        .split("")
+        .reverse()
+        .join("");
     }
   }
-}
+  // mixins: [fruitMixin],
+  // data() {
+  //   return {
+  //     text: "hello there!"
+  //   };
+  // },
+  // components: {
+  //   List
+  // },
+  // filters: {
+  //   toUppercase(value) {
+  //     return value.toUpperCase();
+  //   }
+  // }
+};
 </script>
 
 <style>
-
 </style>
--- filters/src/main.js
+++ filters/src/main.js
@@ -1,18 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+Vue.filter("calculateLength", value => {
+  return value + "(" + value.length + ")";
+});
 
-Vue.filter('to-lowercase', function(value) {
-  return value.toLowerCase();
-})
+// Vue.filter('to-lowercase', function(value) {
+//   return value.toLowerCase();
+// })
 
 // Global mixin used everywhere
-Vue.mixin({
-  created() {
-    console.log('Global Mixin - Created Hook')
-  }
-})
+// Vue.mixin({
+//   created() {
+//     console.log('Global Mixin - Created Hook')
+//   }
+// })
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
